d7009e0402a43f1f1caf222bff4a49700db98601
Phillip Oldham <phillip.oldham@gmail.com>
2016-06-29 11:47:39 +0100

    initial build

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # nameko-beanstalkd
-Beanstalkd dependency for nameko services, based on nameko-redis
+
+Beanstalkd dependency for nameko services
+
+## Installation
+```
+pip install nameko-beanstalkd
+```
+
+## Usage ##
+
+```python
+from nameko.rpc import rpc
+from nameko_beanstalkd import Beanstalkd
+
+
+class MyService(object):
+    name = "my_service"
+
+    beanstalkd = Beanstalkd('development')
+
+    @rpc
+    def hello(self, name):
+        self.beanstalk.put('bob')
+        job = self.beanstalk.reserve()
+        return "Hello, {}!".format(job.body)
+
+```
+To specify beanstalkd connection string you will need a config
+```yaml
+AMQP_URI: 'amqp://guest:guest@localhost'
+BEANSTALKD:
+ development:
+   HOST: '127.0.0.1'
+   PORT: 11300
+```
---
+++ nameko_beanstalkd.py
@@ -0,0 +1,37 @@
+import beanstalkc
+from nameko.extensions import DependencyProvider
+
+BEANSTALKD_KEY = 'BEANSTALKD'
+
+
+class Beanstalkd(DependencyProvider):
+    def __init__(self, key=None):
+        self.key = key
+        self.client = None
+
+    def setup(self):
+        """Set up config params for the connection."""
+        config = self.container.config.get(BEANSTALKD_KEY, {})
+        self.host = config.get(self.key, {}).get('HOST', '0.0.0.0')
+        self.port = config.get(self.key, {}).get('PORT', 11300)
+
+    def start(self):
+        """Set up the connection to beanstalkd."""
+        self.client = beanstalkc.Connection(
+            host=self.host,
+            port=self.port
+        )
+
+    def stop(self):
+        """Close the connection when services stop."""
+        self.client.close()
+        self.client = None
+
+    def kill(self):
+        """Close the connection when services are killed."""
+        self.client.close()
+        self.client = None
+
+    def get_dependency(self, worker_ctx):
+        """Return the client."""
+        return self.client
