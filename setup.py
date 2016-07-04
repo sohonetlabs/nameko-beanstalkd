7fbcce46ca0b50b563ea35b76a23932d2e198713
Marcus Pennington <marcuspen@msn.com>
2016-07-04 10:34:58 +0100

    add anmeko back

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     py_modules=['nameko_beanstalkd'],
     dependency_links=['https://github.com/sohonetlabs/nameko/tarball/master#egg=nameko-2.3.2'],
     install_requires=[
+        "nameko==2.3.2",
         "beanstalkc"
     ],
     description='Beanstalkd dependency for nameko services',
