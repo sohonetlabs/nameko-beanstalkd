01ace392b10b6f0539c87168eb3a293c1ba0fae5
Marcus Pennington <marcuspen@msn.com>
2016-07-04 10:34:03 +0100

    update syntax

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     url='https://github.com/sohonet/nameko-beanstalkd/',
     py_modules=['nameko_beanstalkd'],
-    dependency_links=['https://github.com/sohonetlabs/nameko.git@master#egg=nameko-2.3.2'],
+    dependency_links=['https://github.com/sohonetlabs/nameko/tarball/master#egg=nameko-2.3.2'],
     install_requires=[
         "beanstalkc"
     ],
