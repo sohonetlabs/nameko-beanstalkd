464671b2d31c05f4aee90510f39c327c99397a6b
Marcus Pennington <marcuspen@msn.com>
2016-07-04 10:23:16 +0100

    Update dependency links

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1.0',
     url='https://github.com/sohonet/nameko-beanstalkd/',
     py_modules=['nameko_beanstalkd'],
+    dependency_links=['git+https://git@github.com/sohonetlabs/nameko.git@master#egg=nameko-2.3.2']
     install_requires=[
         "nameko==2.3.2",
         "beanstalkc"
